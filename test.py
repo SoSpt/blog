0f15cad2a9c0b6982bb56a930d27b77ce5d581ac
sophia <sophia.sp@outlook.de>
2021-09-25 13:31:59 +0200

    Meine Django-Girls-App, erster Commit

---
+++ test.py
@@ -0,0 +1,16 @@
+print("hallo")
+a = 4
+x = 7 
+print(x + a)
+
+meine_liste = ["blume", "Baum", "Sonne"]
+print(meine_liste [2])
+#                 größe     farbe   anzahl
+meine_wohnung = ["klein", "blau", 3]
+b_dictionary = {
+    "größe": "klein",
+    "farbe": "blau",
+    "anzahl": 3,
+}
+print(b_dictionary["farbe"])
+
